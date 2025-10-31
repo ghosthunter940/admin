5aa41ca349fd51eaaeee38374a01979463bba4c7
ghosthunter940 <gontermunte@gmail.com>
2025-10-31 13:58:42 +0700

    Update Supabase URL and anon key in client

--- js/lib/supabaseClient.js
+++ js/lib/supabaseClient.js
@@ -3,9 +3,10 @@ import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 
 // Ganti dengan URL dan Kunci Anon Supabase Anda
 // Pastikan ini adalah kredensial yang benar untuk proyek Anda.
-const SUPABASE_URL = 'https://fskpmmkfjzboohejdvfu.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZza3BtbWtmanpib29oZWpkdmZ1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg2MjQwODMsImV4cCI6MjA3NDIwMDA4M30.dZWuyAaf7PdScK3Fgto55GlCcXExeSYHXxgKZJFfKi0';
+const SUPABASE_URL = 'https://uklkjufdtwecjrmvavgv.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVrbGtqdWZkdHdlY2pybXZhdmd2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE4OTIxNDMsImV4cCI6MjA3NzQ2ODE0M30.zB3-EfOqmehI386R7OmOPVl9igHvCR0EoLVjv44OeIw';
 
 // Buat dan EKSPOR koneksi Supabase agar bisa diimpor oleh file lain
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+
 
