0d8dc9d4dd2cca0446fa6f9a892e60396335b7da
ghosthunter940 <gontermunte@gmail.com>
2025-11-06 18:17:54 +0700

    Update products.js

--- js/products.js
+++ js/products.js
@@ -6,102 +6,101 @@ export const localProductList = [
     {
         id: 'beras_regar5kg',
         name: 'Beras Regar 5kg',
-        image_url: 'https://drive.google.com/file/d/1NXj3XNX08xs4aIqfdZ5JasVKbWuC0Gx9/view?usp=drivesdk'
+        image_url: 'asset/beras.jpeg'
     },
     {
         id: 'minyak_bimoli_2l',
         name: 'Minyak Goreng Bimoli 2L',
-        image_url: 'https://drive.google.com/file/d/15aZDW5-DY4fN5yCiPkDkD2KMMOIty9Qv/view?usp=drivesdk'
+        image_url: 'asset/bimoli.jpeg'
     },
     {
         id: 'gula_pasir',
         name: 'Gula Pasir ',
-        image_url: 'https://drive.google.com/file/d/1pF04cCPSncowPo4kxzGz4UiRL90QOFPs/view?usp=drivesdk'
+        image_url: 'asset/gula.jpeg'
     },
     {
         id: 'telur_papan',
         name: 'Telur Ayam 1 Papan (30 butir)',
-        image_url: 'https://drive.google.com/file/d/1-m5ZVdb9QKXU0JTS17psI3sbyzxRXZvM/view?usp=drivesdk'
+        image_url: 'asset/telur.jpeg'
     },
     {
         id: 'mie_indomie_goreng',
         name: 'Indomie Goreng',
-        image_url: 'https://drive.google.com/file/d/1wA3a8AotgarpbB5R2BXqQWm8mD7NKf-4/view?usp=drivesdk'
+        image_url: 'asset/goreng.jpeg'
     },
     {
         id: 'mie_indomie_soto',
         name: 'Indomie Soto',
-        image_url: 'https://drive.google.com/file/d/1ZK0YdNE3Oue3PGzZSL9xzWIaULtUSBcR/view?usp=drivesdk'
+        image_url: 'asset/soto.jpeg'
     },
     {
         id: 'susu_kental_manis',
         name: 'Susu Kental Manis Sachet',
-        image_url: 'https://drive.google.com/file/d/1IpADvrI42typy3LprNVX9LTjSmWY174B/view?usp=drivesdk'
+        image_url: 'asset/manis.jpeg'
     },
     {
         id: 'aqua_gelas',
         name: 'Aqua Gelas 190ml',
-        image_url: 'https://drive.google.com/file/d/17910IzSg24kRrxPj-OD81gQ2_uxnsiav/view?usp=drivesdk'
+        image_url: 'asset/gelas.jpeg'
     },
     {
         id: 'kopi_kapal_api',
         name: 'Kopi Kapal Api Sachet',
-        image_url: 'https://dwwsqycyljpltanlsowp.supabase.co/storage/v1/object/public/products/kopi.jpeg'
+        image_url: 'asset/kopi.jpeg'
     },
     {
         id: 'teh_sariwangi',
         name: 'Teh Sariwangi 25 Teabag',
-        image_url: 'https://dwwsqycyljpltanlsowp.supabase.co/storage/v1/object/public/products/wangi.jpeg'
+        image_url: 'asset/wangi.jpeg'
     },
     {
         id: 'garam_dapur_500g',
         name: 'Garam Dapur 500g',
-        image_url: 'https://dwwsqycyljpltanlsowp.supabase.co/storage/v1/object/public/products/garam.jpeg'
+        image_url: 'asset/garam.jpeg'
     },
     {
         id: 'tepung_terigu_segita',
         name: 'Tepung Terigu Segitiga Biru 1kg',
-        image_url: 'https://dwwsqycyljpltanlsowp.supabase.co/storage/v1/object/public/products/tepung.jpeg'
+        image_url: 'asset/tepung.jpeg'
     },
     {
         id: 'kecap_manis_abc',
         name: 'Kecap Manis ABC 600ml',
-        image_url: 'https://dwwsqycyljpltanlsowp.supabase.co/storage/v1/object/public/products/kecap.jpeg'
+        image_url: 'asset/kecap.jpeg'
     },
     {
         id: 'sambal_abc_botol',
         name: 'Sambal ABC Botol 335ml',
-        image_url: 'https://dwwsqycyljpltanlsowp.supabase.co/storage/v1/object/public/products/sambal.jpeg'
+        image_url: 'asset/sambal.jpeg'
     },
     {
         id: 'rinso_1kg',
         name: 'Deterjen Rinso 1kg',
-        image_url: 'https://dwwsqycyljpltanlsowp.supabase.co/storage/v1/object/public/products/ringso.jpeg'
+        image_url: 'asset/ringso.jpeg'
     },
     {
         id: 'sunlight_755ml',
         name: 'Sunlight Jeruk Nipis 755ml',
-        image_url: 'https://dwwsqycyljpltanlsowp.supabase.co/storage/v1/object/public/products/sunlight.jpeg'
+        image_url: 'asset/sunlight.jpeg'
     },
     {
         id: 'sabun_lifebuoy',
         name: 'Sabun Mandi Lifebuoy Batang',
-        image_url: 'https://dwwsqycyljpltanlsowp.supabase.co/storage/v1/object/public/products/lifebo.jpeg'
+        image_url: 'asset/lifebo.jpeg'
     },
     {
         id: 'shampoo_sunsilk',
         name: 'Shampoo Sunsilk 170ml',
-        image_url: 'https://dwwsqycyljpltanlsowp.supabase.co/storage/v1/object/public/products/sampo.jpeg'
+        image_url: 'asset/sampo.jpeg'
     },
     {
         id: 'pepsodent_190g',
         name: 'Pasta Gigi Pepsodent 190g',
-        image_url: 'https://dwwsqycyljpltanlsowp.supabase.co/storage/v1/object/public/products/pepsoden.jpeg'
+        image_url: 'asset/pepsoden.jpeg'
     },
     {
         id: 'aqua_botol',
         name: 'Air Minum Aqua Botol 600ml',
-        image_url: 'https://dwwsqycyljpltanlsowp.supabase.co/storage/v1/object/public/products/botol.jpeg'
+        image_url: 'asset/botol.jpeg'
     }
 ];
-
